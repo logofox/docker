bd72e00be6787588e1d2856a195832eaeff139cc
Edward.Liu <35363769@qq.com>
2017-08-26 11:16:48 +0800

    修复 路由没有根的BUG

--- main.go
+++ main.go
@@ -13,11 +13,11 @@ func main() {
 		io.WriteString(w, "Hello Docker! "+time.Now().String())
 	})
 
-	http.HandleFunc("hello", func(w http.ResponseWriter, req *http.Request) {
+	http.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
 		io.WriteString(w, "Hello, Welcome! "+time.Now().Format("20060102 15:04:05"))
 	})
 
-	http.HandleFunc("info", func(w http.ResponseWriter, req *http.Request) {
+	http.HandleFunc("/info", func(w http.ResponseWriter, req *http.Request) {
 		if req.Method == http.MethodGet {
 			io.WriteString(w, "Get Info! "+time.Now().Format("20060102 15:04:05"))
 		} else if req.Method == http.MethodPost {
