bc005ca6034e2c966ac36c0c796684f1ef50473e
Edward.Liu <35363769@qq.com>
2017-08-26 11:08:07 +0800

    ADD info search

--- main.go
+++ main.go
@@ -17,6 +17,14 @@ func main() {
 		io.WriteString(w, "Hello, Welcome! "+time.Now().Format("20060102 15:04:05"))
 	})
 
+	http.HandleFunc("info", func(w http.ResponseWriter, req *http.Request) {
+		if req.Method == http.MethodGet {
+			io.WriteString(w, "Get Info! "+time.Now().Format("20060102 15:04:05"))
+		} else if req.Method == http.MethodPost {
+			io.WriteString(w, "Post Info! "+time.Now().Format("20060102 15:04:05"))
+		}
+	})
+
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		fmt.Printf("Err:%v", err)
 	}
